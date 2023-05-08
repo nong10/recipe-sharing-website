00c85d882409aa3685c557a00e99bd53c8f8269d
nong10 <nong10w@outlook.com>
2023-05-08 21:07:55 +0200

    fix delete button,now works with multiple instance

--- components/recipe-brief.php
+++ components/recipe-brief.php
@@ -23,31 +23,36 @@
       } 
     ?>
     </p>
+    <br>
     <?php 
       // display a link to edit-recipes.php with the id of the recipe as button if the page is my-recipes.php
       if ($_SERVER['PHP_SELF'] == "/my-recipes.php") {
         echo "<a href='edit-recipes.php?id=$id' class='button'>Edit</a>";
         echo " ";
-        echo "<button id='delete-recipe' class='button'>Delete</button>";
+        echo "<button class='button' onclick='deleteRecipe($id)'>Delete</button>";
         echo " ";
       }  
     ?>
-  <script>
-    // function to handle  the delete-recipe button
-    document.getElementById('delete-recipe').onclick = function() {
-      // confirm deletion
-      if (confirm("Are you sure you want to delete this recipe?")) {
-        // get the id of the recipe
-        var id = <?php echo $id ?>;
-        // send the id to delete-recipe.php
-      }
-    }
-      
-
-  </script>
 
   </div>
   <div class="recipe-brief-right">
     <img src="<?php echo $image ?>" alt="<?php echo $name ?>">
   </div>
 </div>
+
+<script>
+  // add event listener to delete button
+
+if (typeof deleteRecipe === 'undefined') {
+
+  function deleteRecipe(id) {
+        // get confirmation from user
+        var confirmation = confirm("Are you sure you want to delete this recipe?");
+        if (confirmation) {
+              window.location.assign("php/delete_recipe.php?recipe_id=" + id + "&user_id=<?php echo $_SESSION['user_id'] ?>");
+        }  
+  }
+
+}
+  
+</script> 
